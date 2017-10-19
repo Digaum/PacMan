9d0a8f05fc3398708b9b397e62e334b51ac9a00b
Digaum Spinetti <mrspinetti@gmail.com>
2017-10-19 17:28:28 -0200

    Movimento Pacman com Waypoint

---
+++ PacMan/Assets/Scripts/GhostMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMove : MonoBehaviour {
+
+    public Transform[] destinoGeral;
+    int cont = 0;
+
+    public int posicaoChegou;
+
+    public float speed = 0.3f;
+
+    SpriteRenderer m_SpriteRenderer;
+
+
+    void Start()
+    {
+        //Fetch the SpriteRenderer from the GameObject
+        //m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        //Set the GameObject's Color quickly to a set Color (blue)
+        //m_SpriteRenderer.color = Color.blue;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (transform.position != destinoGeral[cont].position)
+        {
+            Vector2 p = Vector2.MoveTowards(transform.position, destinoGeral[cont].position, speed);
+            GetComponent<Rigidbody2D>().MovePosition(p);
+
+        }
+        else
+        {
+            cont++;
+
+            if (cont == destinoGeral.Length)
+            {
+                cont = 0;
+            }
+        }
+
+
+        Vector2 dir = destinoGeral[cont].position - transform.position;
+        GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GetComponent<Animator>().SetFloat("DirX", dir.y);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D co)
+    {
+        if (co.name == "pacman")
+        {
+            Destroy(co.gameObject);
+        }
+    }
+}
---
+++ PacMan/Assets/Scripts/PacManMove.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacManMove : MonoBehaviour {
+
+    public float speed = 0.4f;
+    Vector2 dest = Vector2.zero;
+
+    public bool power;
+
+	// Use this for initialization
+	void Start () {
+
+        dest = transform.position;
+
+        Debug.Log("Cima: " + Vector2.up);
+        Debug.Log("Baixo: " + Vector2.down);
+        Debug.Log("Direita: " + Vector2.right);
+        Debug.Log("Esquerda: " + Vector2.left);
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+
+
+            if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
+                dest = (Vector2)transform.position + Vector2.up;
+
+            if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
+                dest = (Vector2)transform.position + Vector2.right;
+
+            if (Input.GetKey(KeyCode.DownArrow) && valid(Vector2.down))
+                dest = (Vector2)transform.position + Vector2.down;
+
+            if (Input.GetKey(KeyCode.LeftArrow) && valid(Vector2.left))
+                dest = (Vector2)transform.position + Vector2.left;
+
+
+        Vector2 dir = dest - (Vector2)transform.position;
+        Debug.Log("Direção: " + dir);
+
+        GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GetComponent<Animator>().SetFloat("DirY", dir.y);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            power = true;
+        }
+    }
+
+    bool valid(Vector2 dir)
+    {
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
+}
